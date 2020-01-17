370ce10d847b200b019728a2003b54b38916734c
Balaji Paulrajan <balaji.paulrajan@gmail.com>
2020-01-17 17:20:22 +1100

    diff of squares

---
+++ scala/difference-of-squares/src/main/scala/DifferenceOfSquares.scala
@@ -0,0 +1,8 @@
+object DifferenceOfSquares {
+
+  def sumOfSquares(n: Int): Int = (n * (n+1) * ((2 * n) + 1)) / 6
+
+  def squareOfSum(n: Int): Int = scala.math.pow((n * (n+1))/2,2).toInt
+
+  def differenceOfSquares(n: Int): Int = squareOfSum(n) - sumOfSquares(n)
+}
