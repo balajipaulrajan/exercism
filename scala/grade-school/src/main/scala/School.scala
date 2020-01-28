a2fb04893bda72ac1d9cc837c65d008a4d30aa8e
Balaji Paulrajan <balaji.paulrajan@gmail.com>
2020-01-28 15:52:01 +1100

    grade-school change

--- scala/grade-school/src/main/scala/School.scala
+++ scala/grade-school/src/main/scala/School.scala
@@ -1,20 +1,19 @@
 class School {
   type DB = Map[Int, Seq[String]]
-  private var localStateDB: DB = scala.collection.immutable.TreeMap()
+  private var localStateDB: DB = scala.collection.immutable.TreeMap() 
 
   def add(name: String, g: Int) = {
-    var seq = localStateDB.getOrElse(g, Seq[String]());
+    var seq = localStateDB.getOrElse(g,Seq[String]())
     localStateDB += (g -> (seq :+ name))
   }
 
   def db: DB = localStateDB
 
-  def grade(g: Int): Seq[String] = localStateDB.getOrElse(g, Seq[String]());
+  def grade(g: Int): Seq[String] = localStateDB.getOrElse(g,Seq[String]());
 
   def sorted: DB = {
-    for(key <- localStateDB.keys) {
-      localStateDB += (key -> localStateDB.get(key).map(seq => seq.sorted).get)
-    }
-    localStateDB
+    localStateDB.keySet
+                .map(key => (key -> localStateDB.get(key).map(seq => seq.sorted).get))
+                .toMap
   }
 }
