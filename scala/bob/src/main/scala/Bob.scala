6bcbf0a956c67402d76d410bd1f4b5698539f612
Balaji Paulrajan <balaji.paulrajan@gmail.com>
2020-01-30 11:26:21 +1100

    refactor

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,11 +1,20 @@
 object Bob {
-  def response(statement: String): String = {
-    if (statement.trim.isEmpty) "Fine. Be that way!"
-    else if (statement.trim.last == '?') {
-      if(statement.filter(_.isLetter).isEmpty || !statement.filter(_.isLetter).forall(_.isUpper)) "Sure."
-      else "Calm down, I know what I'm doing!"
+  def response(s: String): String = {
+    val statement = s.trim();
+    if (statement.isEmpty) "Fine. Be that way!"
+    else if (isQuestion(statement)) {
+      if(isShouting(statement)) "Calm down, I know what I'm doing!"
+      else "Sure."
     }
-    else if (!statement.filter(_.isLetter).isEmpty && statement.filter(_.isLetter).forall(_.isUpper)) "Whoa, chill out!"
+    else if (isShouting(statement)) "Whoa, chill out!"
     else "Whatever."
+  }
+
+  def isQuestion(s: String) = s.endsWith("?")
+
+  def isShouting(s: String) = {
+    val letters = s.filter(_.isLetter);
+    val hasLetters = s.exists(_.isLetter);
+    hasLetters && letters.forall(_.isUpper)
   }
 }
