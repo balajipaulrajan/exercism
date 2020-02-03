71b0f82771241ab18f6d7ea383d05cb37a3b57be
Balaji Paulrajan <balaji.paulrajan@gmail.com>
2020-02-04 10:09:22 +1100

    hamming changes to case stmt

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -3,8 +3,7 @@ object Hamming {
   def distance(str1: String, str2: String): Option[Int] = {
     def loop(strList1: List[Char], strList2: List[Char], acc: Int): Option[Int] = (strList1, strList2) match {
       case (Nil, Nil) => Some(acc)
-      case (Nil, _) => None
-      case (_, Nil) => None
+      case (Nil, _) | (_, Nil) => None
       case (x::xs, y::ys) if (x == y) => loop(xs, ys, acc)
       case (x::xs, y::ys) => loop(xs,ys, acc + 1)
     }
